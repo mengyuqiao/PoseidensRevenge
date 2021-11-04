7d986d5aed5dbfb021c054f0f78137f82f379f2f
mengyuqiao <1156253960@qq.com>
2021-11-04 19:07:12 -0400

    Initial commit

---
+++ android/src/com/mygdx/poseidensrevenge/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.poseidensrevenge;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.poseidensrevenge.PoseidensRevenge;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PoseidensRevenge(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/poseidensrevenge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.poseidensrevenge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.poseidensrevenge.PoseidensRevenge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PoseidensRevenge(), config);
+	}
+}
